00ad6c260f66a251a6d26585fb41d22e5f33ed2a
bjd202 <bjd202@naver.com>
2020-06-29 22:47:54 +0900

    second

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
+
+const {User} = require('./models/User');
 
 //mongoDB
 mongoose.Promise = global.Promise;
@@ -14,6 +22,16 @@ mongoose.connect('mongodb://localhost:27017/react-basic', { useNewUrlParser: tru
     console.error(err);
 })
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!!!'))
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success : true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
