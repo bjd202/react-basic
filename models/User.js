5f97963633adef35325dd42368f628de6bc8cd84
bjd202 <bjd202@naver.com>
2020-06-30 23:09:51 +0900

    200630

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name : {type : String, maxlegth : 50},
@@ -10,6 +13,60 @@ const userSchema = mongoose.Schema({
     token : {type : String},
     tokenExp : {type : Number}
 })
+
+userSchema.pre('save', function(next){
+    var user = this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err);
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            });
+        });
+    }else{
+        next();
+    }
+})
+
+userSchema.methods.comparePassword = function (plainPassword, callback) {
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        
+        if(err) return callback(err)
+
+        callback(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function (callback) {
+    var user = this;
+    console.log(user);
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+    console.log(user._id.toHexString());
+
+    user.token = token;
+    user.save(function (err, user) {
+        console.log(err)
+        if(err) return callback(err)
+
+        callback(null, user);
+    })
+}
+
+userSchema.statics.findByToken = function (token, callback) {
+    var user = this;
+
+    jwt.verify(token, "secretToken", function (err, decoded) {
+        user.findOne({"_id" : decoded, "token" : token}, function (err, user) {
+            if(err) return callback(err);
+
+            callback(null, user);
+        })
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
